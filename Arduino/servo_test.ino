0f2dc03114b684d935e216e0ddea220b2955169d
佐藤佑亮 <310yusuke@satouyuusukenoMacBook-Air.local>
2021-10-28 15:33:45 +0900

    two servos

--- Arduino/servo_test.ino
+++ Arduino/servo_test.ino
@@ -38,12 +38,16 @@
 
 #include <ESP32Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo01;// create servo object to control a servo
+Servo myservo02;
+//Servo myservo03;
 // 16 servo objects can be created on the ESP32
 
 int pos = 0;    // variable to store the servo position
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
 int servoPin = 2;
+int servoPin02 = 0;
+//int servoPin03 = @@;
 
 void setup() {
   Serial.begin(38400);
@@ -52,8 +56,12 @@ void setup() {
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
-	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+	myservo01.setPeriodHertz(50);    // standard 50 hz servo
+  myservo02.setPeriodHertz(50);
+  //myservo03.setPeriodHertz(50);
+	myservo01.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+	myservo02.attach(servoPin02, 1000, 2000);
+	//myservo03.attach(servoPin03, 1000, 2000);
 	// using default min/max of 1000us and 2000us
 	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
@@ -63,12 +71,16 @@ void loop() {
 
 	for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 		// in steps of 1 degree
-		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		myservo01.write(pos);    // tell servo to go to position in variable 'pos'
+    myservo02.write(pos);
+    //myservo03.write(pos);
 		Serial.println(pos);
 		delay(15);             // waits 15ms for the servo to reach the position
 	}
 	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		myservo01.write(pos);    // tell servo to go to position in variable 'pos'
+    myservo02.write(pos);
+    //myservo03.write(pos);
 		delay(15);             // waits 15ms for the servo to reach the position
 	}
 }

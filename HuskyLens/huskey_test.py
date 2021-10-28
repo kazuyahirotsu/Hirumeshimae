522900a0f29cfaff9b6c78dd59da6f82cdb5e12b
RinTrin <tomirin.rinta@gmail.com>
2021-10-28 17:40:13 +0900

    to_i2c

--- HuskyLens/huskey_test.py
+++ HuskyLens/huskey_test.py
@@ -1,5 +1,6 @@
 from huskylib import HuskyLensLibrary
-hl = HuskyLensLibrary("SERIAL", "/dev/ttyUSB0")
+# hl = HuskyLensLibrary("I2C", "/dev/ttyUSB0")
+hl = HuskyLensLibrary("I2C","", address=0x32, channel=0)
 print(h1.knock())
 
 
